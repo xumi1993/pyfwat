2ef456c74f7b2e7cf8eacdad6e47020e52481ac7
Mijian Xu <mijian.xu@smail.nju.edu.cn>
2022-02-22 16:01:51 +0800

    add save semd

--- gen_gauss_stf.py
+++ gen_gauss_stf.py
@@ -6,14 +6,15 @@ def gauss(x, a=1, b=0, c=2):
     return a*np.exp(-(x-b)**2/2*c**2)
 
 
-def create_stf(npts=4800, dt=0.025, shift=10, a=1e-5, b=50, c=1):
+def create_stf(npts=1500, dt=0.04, shift=6, a=1e-5, b=20, c=0.6):
     x = np.arange(0, npts)*dt - shift
     y = gauss(x, a, b, c)
     sac = SACTrace(data=y, b=-shift, delta=dt)
-    with open('src_rec/sources_ls.dat.tele') as f:
-        for line in f.readlines():
-            label = line.strip().split()[0]
-            sac.write('src_rec/STF_{}.sac'.format(label))
+    sac.write('src_rec/STF_13.sac')
+    # with open('src_rec/sources_ls.dat.tele') as f:
+    #     for line in f.readlines():
+    #         label = line.strip().split()[0]
+    #         sac.write('src_rec/STF_{}.sac'.format(label))
 
 
 if __name__ == '__main__':
--- plot_seis.py
+++ plot_seis.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from time import time
 import numpy as np
 from os.path import join
 import sys
@@ -6,6 +7,7 @@ from utils import *
 from seispy.geo import rotateSeisENtoTR
 import matplotlib.pyplot as plt
 from obspy.signal.filter import bandpass, highpass, lowpass
+from obspy.io.sac import SACTrace
 
 
 def get_stations(basepath):
@@ -19,10 +21,10 @@ def read_tr(basepath, comp='z', filter=True, unit='d'):
     stations, network, x, y, z = get_stations(basepath)
     npts = int(readpar(join(basepath, 'DATA', 'Par_file'), 'NSTEP'))
     st = np.zeros([x.shape[0], npts])
-    time_axis = np.loadtxt(join(basepath, 'OUTPUT_FILES', network[0]+'.'+stations[0]+'.CX'+comp.upper()+'.sem'+unit), usecols=[0,])
+    time_axis = np.loadtxt(join(basepath, 'OUTPUT_FILES', network[0]+'.'+stations[0]+'.BX'+comp.upper()+'.sem'+unit), usecols=[0,])
     dt = np.mean(np.diff(time_axis))
     for i, staname in enumerate(stations):
-        fname = join(basepath, 'OUTPUT_FILES', network[i]+'.'+staname+'.CX'+comp.upper()+'.sem'+unit)
+        fname = join(basepath, 'OUTPUT_FILES', network[i]+'.'+staname+'.BX'+comp.upper()+'.sem'+unit)
         st[i] = np.loadtxt(fname, usecols=[1,])
         if filter:
             st[i] = lowpass(st[i], 1, 1/dt)
@@ -49,7 +51,7 @@ def single_draw(ax, time_axis, stx, data, enf=0.5):
 
 
 def draw(basepath):
-    plt.figure(figsize=(16, 8))
+    plt.figure(figsize=(16, 8), dpi=300)
     axr = plt.subplot(1, 3, 1)
     axr.set_title('R comp')
     axt = plt.subplot(1, 3, 2)
@@ -58,7 +60,7 @@ def draw(basepath):
     axz.set_title('Z comp')
     _, _, x, _, _ = get_stations(basepath)
     for ax in [axr, axt, axz]:
-        ax.grid(b=True, axis='x')
+        ax.grid(True, axis='x')
         ax.set_ylim([x[0]/1000-5,x[-1]/1000+5])
     time_axis, zst = read_tr(basepath, comp='z')
     tst, rst = rotate(basepath)
@@ -68,7 +70,27 @@ def draw(basepath):
     plt.savefig(join(basepath, 'OUTPUT_FILES', 'waves_rtz.png'), bbox_inches='tight')
 
 
+def save(time_axis, data, comp, sta, net, path='./'):
+    sac = SACTrace(data=data)
+    sac.b = time_axis[0]
+    sac.delta = np.mean(np.diff(time_axis))
+    sac.kcmpnm = 'BX{}'.format(comp)
+    sac.kstnm = sta
+    sac.knetwk = net
+    sac.write('{}/{}.{}.BX{}.syn'.format(path, net, sta, comp))
+
+
+def savesemd(basepath='./', outpath='OUTPUT_FILES'):
+    sta, net, x, _, _ = get_stations(basepath)
+    time_axis, zst = read_tr(basepath, comp='z')
+    tst, rst = rotate(basepath)
+    for i, staname in enumerate(sta):
+        save(time_axis, rst[i], 'R', staname, net[i], outpath)
+        save(time_axis, zst[i], 'Z', staname, net[i], outpath)
+
+
 if __name__ == "__main__":
     basepath = sys.argv[1]
     # basepath = 'benchmark_line_baz0_inc33.3489'
     draw(basepath)
+    savesemd(basepath)
