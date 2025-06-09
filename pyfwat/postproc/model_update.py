bc6fcf9e798f26af026f8086701f162ef92a32d9
xumi1993 <gomijianxu@gmail.com>
2025-06-09 18:22:53 -0400

    update model

--- pyfwat/postproc/model_update.py
+++ pyfwat/postproc/model_update.py
@@ -7,18 +7,19 @@ import argparse
 
 
 data_names = ['vp', 'vs', 'rho']
-model_h5 = 'DATA/tomo_files/tomography_xyz.h5'
+model_h5 = 'DATA/tomo_files/tomography_model.h5'
 
 class ModUpdate():
     def __init__(self, model_name, step_length):
+        self.para = readfwatpar()
         self.step_length = step_length
         self.model_name = model_name
         self.iter_current = int(model_name[1:])
         self.iter_next = self.iter_current + 1
-        self.iter_start = int(readfwatpar('DATA/FWAT.PAR', 'ITER_START'))
-        self.m_store = int(readfwatpar('DATA/FWAT.PAR', 'LBFGS_M_STORE'))
-        self.do_ls = readfwatpar('DATA/FWAT.PAR', 'DO_LS')
-        self.optim_method = readfwatpar('DATA/FWAT.PAR', 'OPT_METHOD')
+        self.iter_start = self.para['MODEL_UPDATE']['ITER_START']
+        self.m_store = self.para['MODEL_UPDATE']['LBFGS_M_STORE']
+        self.do_ls = self.para['MODEL_UPDATE']['DO_LS']
+        self.optim_method = self.para['MODEL_UPDATE']['OPT_METHOD']
         self.read_coord()
 
     def lbfgs(self):
