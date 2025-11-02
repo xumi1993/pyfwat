3903ca540c702ccc163c00a68175fdfe2ba637d3
xumi1993 <gomijianxu@gmail.com>
2025-11-02 15:06:13 -0500

    add writing gll file

--- pyfwat/io/gllio.py
+++ pyfwat/io/gllio.py
@@ -28,20 +28,55 @@ def read_fortran_model(filename, nspec_ab, ngllx=5, nglly=5, ngllz=5):
         return data
 
 
+def write_fortran_model(filename, data):
+    """Write model data to binary file with Fortran order.
+    
+    Parameters
+    ----------
+    filename : str
+        Output filename for the binary file
+    data : numpy.ndarray
+        Model data array with shape (ngllx, nglly, ngllz, nspec_ab)
+        in Fortran order
+    ngllx : int, optional
+        Number of GLL points in x direction, by default 5
+    nglly : int, optional
+        Number of GLL points in y direction, by default 5
+    ngllz : int, optional
+        Number of GLL points in z direction, by default 5
+    """
+    # Flatten the data in Fortran order
+    data_flat = data.flatten(order='F').astype('float32')
+    
+    # Calculate record length in bytes
+    record_length = data_flat.nbytes
+    
+    with open(filename, "wb") as file:
+        # Write header (4-byte integer for record length)
+        np.array([record_length], dtype='int32').tofile(file)
+        # Write data
+        data_flat.tofile(file)
+        # Write footer (4-byte integer for record length)
+        np.array([record_length], dtype='int32').tofile(file)
+
+
 class GllModel:
-    def __init__(self, inpath, parameters=['vp', 'vs', 'rho'], ngllx=5, nglly=5, ngllz=5) -> None:
-        self.inpath = inpath
-        self.parameters = parameters
+    def __init__(self, mesh_path, volumn_path, volumns=['vp', 'vs', 'rho'], ngllx=5, nglly=5, ngllz=5) -> None:
+        self.mesh_path = mesh_path
+        self.volumn_path = volumn_path
         self.model_data = {}
-        for para in parameters:
+        if isinstance(volumns, str):
+            volumns = [volumns]
+        self.parameters = volumns
+        for para in volumns:
             self.model_data[para] = []
         self.nglls = {'ngllx': ngllx,
                       'nglly': nglly,
                       'ngllz': ngllz}
         self.read_external_mesh()
-        self.get_minmax()
+        self._get_minmax()
 
-    def get_minmax(self):
+    def _get_minmax(self):
         self.xmin = np.min([np.min(ex['xstore']) for ex in self.external_meshs])
         self.xmax = np.max([np.max(ex['xstore']) for ex in self.external_meshs])
         self.ymin = np.min([np.min(ex['ystore']) for ex in self.external_meshs])
@@ -50,9 +85,10 @@ class GllModel:
         self.zmax = np.max([np.max(ex['zstore']) for ex in self.external_meshs])
 
     def read_external_mesh(self):
+        """ read external mesh and model data from files. """
         self.external_meshs = []
         extbins = glob(os.path.join(
-            self.inpath, 'proc*_external_mesh.bin')
+            self.mesh_path, 'proc*_external_mesh.bin')
         )
         self.nproc = len(extbins)
         for _, extbin in enumerate(extbins):
@@ -60,13 +96,32 @@ class GllModel:
             mesh_data = read_fortran_external_mesh(extbin, **self.nglls)
             self.external_meshs.append(mesh_data)
             for para in self.parameters:
-                fname = os.path.join(self.inpath, 'proc{}_{}.bin'.format(pname, para))
+                fname = os.path.join(self.volumn_path, 'proc{}_{}.bin'.format(pname, para))
                 self.model_data[para].append(read_fortran_model(fname, mesh_data['nspec_ab'], **self.nglls))
 
     def get_points_data(self):
         self.points = np.empty([0, 3+len(self.parameters)])
         for i in range(self.nproc):
             self.points = np.vstack((self.points, self._get_gll_point(i)))
+
+    def write(self, output_path, key=None):
+        """ Write model data back to Fortran binary files.
+        
+        Parameters
+        ----------
+        output_path : str
+            Directory path to save the output binary files.
+        """
+        os.makedirs(output_path, exist_ok=True)
+        if key is None:
+            fields = self.parameters
+        else:
+            fields = [key]
+        for i in range(self.nproc):
+            pname = f"{i:06d}"
+            for para in fields:
+                fname = os.path.join(output_path, f'proc{pname}_{para}.bin')
+                write_fortran_model(fname, self.model_data[para][i])
 
     def griddata(self, x, y, z, method='linear'):
         """ grid data with given series of x, y, z.
